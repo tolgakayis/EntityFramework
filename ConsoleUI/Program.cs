531b05a2f5d73781accbb05665b6d0d44b0e6a6b
Tolga Kayis <tolgakayis_@hotmail.com>
2023-11-13 01:13:13 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+//7. Gün
+// SOLID
+// Open Closed Principle
+
+ProductManager productManager = new ProductManager(new EfProductDal());
+
+//foreach (var product in productManager.GetAll())
+//{
+//    Console.WriteLine(product.ProductName);
+//}
+
+//foreach (var product in productManager.GetAllByCategoryId(1))
+//{
+//	Console.WriteLine(product.ProductName);
+//}
+
+foreach (var product in productManager.GetByUnitPrice(30, 50))
+{
+	Console.WriteLine(product.ProductName + "---" + product.UnitPrice);
+}
